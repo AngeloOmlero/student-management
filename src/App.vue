3ec8ae2725e499815284acaf7804f334b20dd7dd
Angelo Omlero <aomlero@vauldex.com>
2025-11-12 11:43:39 +0800

    declare global font

--- src/App.vue
+++ src/App.vue
@@ -11,4 +11,13 @@ const route = useRoute()
     <RouterView />
   </div>
 </template>
+<style>
+/* src/assets/main.css or global.css */
+@import url('https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;500;600;700&display=swap');
+
+*{
+   font-family: 'Fira Sans', sans-serif;
+}
+
+</style>
 
